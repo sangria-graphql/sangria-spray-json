a622261cdc736e2974dec4e6599c6f034e2dfc94
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2016-01-23 18:22:10 +0100

    Rendering and exception tests

--- src/test/scala/sangria/marshalling/SprayJsonSupportSpec.scala
+++ src/test/scala/sangria/marshalling/SprayJsonSupportSpec.scala
@@ -25,4 +25,51 @@ class SprayJsonSupportSpec extends WordSpec with Matchers with MarshallingBehavi
     behave like `case class input marshaller` (SprayJsonResultMarshaller)
   }
 
+  val toRender = JsObject(
+    "a" → JsArray(JsNull, JsNumber(123), JsArray(JsObject("foo" → JsString("bar")))),
+    "b" → JsObject(
+      "c" → JsBoolean(true),
+      "d" → JsNull))
+
+  "InputUnmarshaller" should {
+    "throw an exception on invalid scalar values" in {
+      an [IllegalStateException] should be thrownBy
+          SprayJsonInputUnmarshaller.getScalarValue(JsObject.empty)
+    }
+
+    "throw an exception on variable names" in {
+      an [IllegalArgumentException] should be thrownBy
+          SprayJsonInputUnmarshaller.getVariableName(JsString("$foo"))
+    }
+
+    "render JSON values" in {
+      val rendered = SprayJsonInputUnmarshaller.render(toRender)
+
+      rendered should be ("""{"a":[null,123,[{"foo":"bar"}]],"b":{"c":true,"d":null}}""")
+    }
+  }
+
+  "ResultMarshaller" should {
+    "render pretty JSON values" in {
+      val rendered = SprayJsonResultMarshaller.renderPretty(toRender)
+
+      println(rendered)
+      rendered.replaceAll("\r", "") should be (
+        """{
+          |  "a": [null, 123, [{
+          |    "foo": "bar"
+          |  }]],
+          |  "b": {
+          |    "c": true,
+          |    "d": null
+          |  }
+          |}""".stripMargin.replaceAll("\r", ""))
+    }
+
+    "render compact JSON values" in {
+      val rendered = SprayJsonResultMarshaller.renderCompact(toRender)
+
+      rendered should be ("""{"a":[null,123,[{"foo":"bar"}]],"b":{"c":true,"d":null}}""")
+    }
+  }
 }
